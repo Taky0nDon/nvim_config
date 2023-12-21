0eea9cbc8f495cf33a9d427bc288eef41c8fceef
Taky0nDon <126304313+Taky0nDon@users.noreply.github.com>
2023-12-21 09:37:07 -0500

    call config.lazy in init.vim for lazy setup

--- init.lua
+++ init.lua
@@ -1,28 +1,5 @@
--- Load keymappings
 require("config.keymaps")
-local plugins = require("plugins.plugins")
-local colors = require("colors.colors")
--- Lazy setup
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
-end
-vim.opt.rtp:prepend(lazypath)
-
--------------------------- LOAD PLUGINS ----------------------------------------
--- local plugs = require("plugins.plugins")
-require("lazy").setup({
-	plugins,
-	colors,
-})
-
+require("config.lazy")
 require("config.options")
 require("config.autocmds")
 require("config.lsp_config")
