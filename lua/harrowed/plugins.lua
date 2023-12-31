b9f26216de790f1cd90f46119ac2d7418346fd75
Taky0ndon <char@za.rd>
2023-12-30 22:42:55 -0500

    installed lsp-zero plugins

--- lua/harrowed/plugins.lua
+++ lua/harrowed/plugins.lua
@@ -1,13 +1,35 @@
-print("harrowed.plugins accessed")
+local lsp_support = {
+    {"neovim/nvim-lspconfig"},
+    {"williamboman/mason.nvim"},
+    {"williamboman/mason-lspconfig.nvim"},
+}
+local completion = {
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "saadparwaiz1/cmp_luasnip",
+    "hrsh7th/cmp-nvim-lua",
+}
+
+local snippets = {
+    "L3MON4D3/LuaSnip",
+    "rafamadriz/friendly-snippets",
+}
+
+local lsp_zero = {
+    lsp_support,
+    completion,
+    snippets,
+}
+
 return {
     { "rebelot/kanagawa.nvim" },
     { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
     { "miikanissi/modus-themes.nvim" },
     { "nvim-telescope/telescope.nvim", dependencies = { 'nvim-lua/plenary.nvim' } },
-    {"neovim/nvim-lspconfig"},
-    {"williamboman/mason.nvim"},
-    {"williamboman/mason-lspconfig.nvim"},
     {"theprimeagen/harpoon"},
     {"mbbill/undotree"},
-    {"tpope/vim-fugitive"}
+    {"tpope/vim-fugitive"},
+    {lsp_zero},
 }
