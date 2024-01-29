805096b38ffa63a5e7de7d1594a1086bf3d96110
Taky0ndon <char@za.rd>
2024-01-28 22:00:37 -0500

    added incremental selection and textobject treesitter mdoules

--- lua/harrowed/plugins.lua
+++ lua/harrowed/plugins.lua
@@ -1,9 +1,9 @@
-local vim_be_good = {"ThePrimeagen/vim-be-good"}
+local vim_be_good = { "ThePrimeagen/vim-be-good" }
 
 local lsp_support = {
-    {"neovim/nvim-lspconfig"},
-    {"williamboman/mason.nvim"},
-    {"williamboman/mason-lspconfig.nvim"},
+    { "neovim/nvim-lspconfig" },
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
 }
 local completion = {
     "hrsh7th/nvim-cmp",
@@ -27,11 +27,66 @@ local lsp_zero = {
 }
 
 return {
-    {vim_be_good},
+    { vim_be_good },
     { "rebelot/kanagawa.nvim" },
-    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
-        config = function ()
-            require'nvim-treesitter.configs'.setup {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require 'nvim-treesitter.configs'.setup {
+                textobjects = {
+                    select = {
+                        enable = true,
+
+                        -- Automatically jump forward to textobj, similar to targets.vim
+                        lookahead = true,
+
+                        keymaps = {
+                            -- You can use the capture groups defined in textobjects.scm
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["ac"] = "@class.outer",
+                            -- You can optionally set descriptions to the mappings (used in the desc parameter of
+                            -- nvim_buf_set_keymap) which plugins like which-key display
+                            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                            -- You can also use captures from other query groups like `locals.scm`
+                            ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+                        },
+                        -- You can choose the select mode (default is charwise 'v')
+                        --
+                        -- Can also be a function which gets passed a table with the keys
+                        -- * query_string: eg '@function.inner'
+                        -- * method: eg 'v' or 'o'
+                        -- and should return the mode ('v', 'V', or '<c-v>') or a table
+                        -- mapping query_strings to modes.
+                        selection_modes = {
+                            ['@parameter.outer'] = 'v', -- charwise
+                            ['@function.outer'] = 'V', -- linewise
+                            ['@class.outer'] = '<c-v>', -- blockwise
+                        },
+                        -- If you set this to `true` (default is `false`) then any textobject is
+                        -- extended to include preceding or succeeding whitespace. Succeeding
+                        -- whitespace has priority in order to act similarly to eg the built-in
+                        -- `ap`.
+                        --
+                        -- Can also be a function which gets passed a table with the keys
+                        -- * query_string: eg '@function.inner'
+                        -- * selection_mode: eg 'v'
+                        -- and should return true of false
+                        include_surrounding_whitespace = true,
+                    },
+                },
+
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<leader>ss",
+                        node_incremental = "<leader>sp",
+                        scope_incremental = "<leader>sS",
+                        node_decremental = "<leader>sc",
+                    },
+                },
+
                 ensure_installed = { "python", "perl", "bash", "c", "lua", "vim", "vimdoc", "query" },
                 -- Install parsers synchronously (only applied to `ensure_installed`)
                 sync_install = false,
@@ -39,18 +94,20 @@ return {
                 -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
                 auto_install = true,
                 highlight = {
-                  enable = true,
-                  disable = {},
-                  additional_vim_regex_highlighting = false,
+                    enable = true,
+                    disable = {},
+                    additional_vim_regex_highlighting = false,
                 },
             }
-        end},
+        end
+    },
+    { "nvim-treesitter/nvim-treesitter-textobjects" },
     { "miikanissi/modus-themes.nvim" },
-    { "nvim-telescope/telescope.nvim", dependencies = { 'nvim-lua/plenary.nvim' } },
-    {"theprimeagen/harpoon"},
-    {"mbbill/undotree"},
-    {"tpope/vim-fugitive"},
-    {lsp_zero},
-    {"folke/which-key.nvim"},
-    {"ms-jpq/coq_nvim", branch="coq"},
+    { "nvim-telescope/telescope.nvim",              dependencies = { 'nvim-lua/plenary.nvim' } },
+    { "theprimeagen/harpoon" },
+    { "mbbill/undotree" },
+    { "tpope/vim-fugitive" },
+    { lsp_zero },
+    { "folke/which-key.nvim" },
+    { "ms-jpq/coq_nvim",                            branch = "coq" },
 }
